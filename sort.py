b80df6c461ee6ed9f86456c09ba87cc8f962bf08
kapil780 <55044629+kapil780@users.noreply.github.com>
2020-05-02 14:03:25 +0530

    Create sort.py
    Sort files

---
+++ sort.py
@@ -0,0 +1,44 @@
+import os
+
+
+def createFolder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+def move(foldername, files):
+    for file in files:
+        os.replace(file, f"{foldername}/{file}")
+
+
+if __name__ == '__main__':
+    files = os.listdir()
+    files.remove("sort_files.py")
+    print(files)
+    createFolder("Images")
+    createFolder("Documents")
+    createFolder("Medias")
+    createFolder("Others")
+    imgExts = [".png", ".jpeg", ".jpg"]
+    images = [file for file in files if os.path.splitext(file)[1].lower() in imgExts]
+
+    docExts = [".txt", ".pdf", ".docx"]
+    documents = [file for file in files if os.path.splitext(file)[1].lower() in docExts]
+
+    medExts = [".mp3", ".mp4", ".mkv"]
+    medias = [file for file in files if os.path.splitext(file)[1].lower() in medExts]
+
+    print(images, documents, medias)
+
+    others = []
+    for file in files:
+        ext = os.path.splitext(file)[1].lower()
+        if (ext not in imgExts) and (ext not in docExts) and (ext not in medExts) and os.path.isfile(file):
+            others.append(file)
+
+    print(others)
+
+    move("Medias", medias)
+    move("Images", images)
+    move("Documents", documents)
+    move("Others", others)
